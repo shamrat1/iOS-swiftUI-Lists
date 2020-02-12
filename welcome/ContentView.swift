230abb8c968f29d8abb87cb6df4c4483c97db619
Tanvir  Ahmed <Ta>
2020-02-11 22:43:04 -0800

    Initial Commit

---
+++ welcome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  welcome
+//
+//  Created by Mac on 2/11/20.
+//  Copyright © 2020 JaKhushiTai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
